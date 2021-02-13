e7ecdff43b8569f23e47af8b4ef7f347ad561e96
Michael Azzopardi <michael.azzopardi@creative-innovation.net>
2021-02-13 16:55:57 +0100

    Initial prototype

--- install/local_setup.sh
+++ install/local_setup.sh
@@ -6,7 +6,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/media/michael/bach_SD_128GB/emo2/emo2.0/install"
+_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/media/michael/bach_SD_128GB/emo_ws/emo2.0/install"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_prefix_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_prefix_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
--- src/emo_proto/setup.py
+++ src/emo_proto/setup.py
@@ -4,22 +4,39 @@ package_name = 'emo_proto'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
+
+    install_requires=['setuptools'],
+    zip_safe=True,
+
+    author='Michael Azzopardi',
+    author_email='michael.azzopardi@creative-innovation.net',
+    maintainer='Michael Azzopardi',
+    maintainer_email='michael.azzopardi@creative-innovation.net',
+    
+    keywords=['ROS','ROS2'],    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software Licencse',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+
+    description='EMO 2.0 prototyping package for testing concepts and constructs.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    
+    entry_points={
+        'console_scripts': [
+            'proto = emo_proto.emo_proto_publisher:main',
+        ],
+    },
+
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='michael',
-    maintainer_email='michael@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    
 )
